e4a2c978d94c876d92e1be104ff7e344a4f09163
wonderdeath <39296168+wonderdeath@users.noreply.github.com>
2019-06-13 17:33:53 +0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,1 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
-firebase.initializeApp({
-    messagingSenderId: '568207334148'
-});
-
-const messaging = firebase.messaging();
